ea9dd6f8b5dabc24916692ca404f0f31e5cfb511
Daniil Portnov <danila.portnov.wade@gmail.com>
2021-08-12 16:12:56 +0300

    done phone

--- tasks/phone.py
+++ tasks/phone.py
@@ -17,3 +17,27 @@
 Модель: {}
 Год выпуска: {}
 """
+
+
+class Phone:
+    brand: str
+    model: str
+    issue_year: int
+    name: str
+
+    def __init__(self, brand: str, model: str, issue_year: int, name: str):
+        self.brand = brand
+        self.model = model
+        self.issue_year = issue_year
+        self.name = name
+
+    def receive_call(self):
+        return f" Звонит {self.name}"
+
+    def __str__(self):
+        return f" Бренд: {self.brand}\n Модель: {self.model}\n Год выпуска: {self.issue_year}"
+
+
+phone = Phone("Apple", "Iphone7", 2018, "Сергей")
+print(phone)
+print(phone.receive_call())
